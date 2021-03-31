7178ef4516376d0f12ddd771346ae6edb4f2092d
arsd <fkhlil@mail.arsd.dc>
2021-03-31 16:57:10 +0200

    basic: add javascript

---
+++ basics/main.js
@@ -0,0 +1,16 @@
+const listrespos = async username => {
+    const repos = await fetch(`https://api.github.com/users/${username}/repos?type=owner&sort=updated`)
+                .then(res => res.json())
+                .catch(error => console.log(error))
+}
+
+const markup = repos
+                        .map(
+                            repo => `
+                                <li>
+                                    <a href="${repo.html_url}" > ${repo.name} </a>
+                                    (  )
+                                </li>
+                            
+                            `
+                        )
